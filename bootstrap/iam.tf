511a58f1de3d0d4c5a131c48295f81561de15a9e
Jay P <jayasunderam@gmail.com>
2026-03-05 14:19:39 -0800

    add bootstrap

---
+++ bootstrap/iam.tf
@@ -0,0 +1,55 @@
+resource "aws_iam_role" "github_actions_role" {
+  name = "github-actions-terraform"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+
+        Action = "sts:AssumeRoleWithWebIdentity"
+
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:${var.github_org}/${var.github_repo}:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "terraform_admin" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
+}
+
+
+resource "aws_iam_role_policy" "github_actions_iam_bridge" {
+  name = "github-actions-iam-bridge"
+  role = aws_iam_role.github_actions_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "iam:GetRole",
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:PutRolePolicy",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PassRole",
+          "iam:TagRole"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
---
+++ bootstrap/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "github_org" {
+  description = "GitHub organization or username"
+}
+
+variable "github_repo" {
+  description = "Repository name"
+}
